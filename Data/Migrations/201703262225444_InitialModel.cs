b8ef6bf48c1d5461d6e2c313e720a2eddd952846
katraguz <katarinar11@gmail.com>
2017-03-27 00:31:53 +0200

    Add InitialModel migration

---
+++ Data/Migrations/201703262225444_InitialModel.cs
@@ -0,0 +1,61 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 30),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 30),
+                        Surname = c.String(nullable: false, maxLength: 30),
+                        Email = c.String(nullable: false, maxLength: 30),
+                        Password = c.String(nullable: false, maxLength: 255),
+                        PhoneNumber = c.String(nullable: false, maxLength: 15),
+                        BirthDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Email, unique: true);
+            
+            CreateTable(
+                "dbo.UserRoles",
+                c => new
+                    {
+                        User_Id = c.Int(nullable: false),
+                        Role_Id = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.User_Id, t.Role_Id })
+                .ForeignKey("dbo.Users", t => t.User_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Roles", t => t.Role_Id, cascadeDelete: true)
+                .Index(t => t.User_Id)
+                .Index(t => t.Role_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.UserRoles", "Role_Id", "dbo.Roles");
+            DropForeignKey("dbo.UserRoles", "User_Id", "dbo.Users");
+            DropIndex("dbo.UserRoles", new[] { "Role_Id" });
+            DropIndex("dbo.UserRoles", new[] { "User_Id" });
+            DropIndex("dbo.Users", new[] { "Email" });
+            DropTable("dbo.UserRoles");
+            DropTable("dbo.Users");
+            DropTable("dbo.Roles");
+        }
+    }
+}
