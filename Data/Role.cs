5ea00524736e230852fe1f874ca7baee57a8889d
katraguz <katarinar11@gmail.com>
2017-03-26 23:47:36 +0200

    Add Data annotations to User and Role classes

--- Data/Role.cs
+++ Data/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Data
     public class Role
     {
         public byte Id { get; set; }
+        [Required, StringLength(30)]
         public string Name { get; set; }
         public ICollection<User> Users { get; set; }
     }
--- Data/User.cs
+++ Data/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +11,17 @@ namespace Data
     public class User
     {
         public int Id { get; set; }
+        [Required, StringLength(30)]
         public string Name { get; set; }
+        [Required, StringLength(30)]
         public string Surname { get; set; }
+        [Required, StringLength(30), Index(IsUnique = true)]
         public string Email { get; set; }
+        [Required, StringLength(255)]
         public string Password { get; set; }
+        [Required, StringLength(15)]
         public string PhoneNumber { get; set; }
+        [Required]
         public DateTime BirthDate { get; set; }
         public ICollection<Role> Roles { get; set; }
     }
