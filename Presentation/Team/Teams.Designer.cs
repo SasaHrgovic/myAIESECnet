42794131d20e86cdd075743f24e6c6843cea8da6
Saša Hrgović <shrgovic@outlook.com>
2017-04-30 15:17:31 +0200

    Add Teams User Control
    This user control will display a list of teams.
    Could be upgraded to show list of members for a selected team.

---
+++ Presentation/Team/Teams.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Presentation.Team
+{
+    partial class Teams
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTeams = new System.Windows.Forms.DataGridView();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnAddTeam = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTeams)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTeams
+            // 
+            this.dgvTeams.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTeams.Location = new System.Drawing.Point(35, 70);
+            this.dgvTeams.Name = "dgvTeams";
+            this.dgvTeams.Size = new System.Drawing.Size(1056, 238);
+            this.dgvTeams.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTitle.Location = new System.Drawing.Point(31, 34);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(257, 24);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Popis timova lokalnog odbora";
+            // 
+            // btnAddTeam
+            // 
+            this.btnAddTeam.Location = new System.Drawing.Point(1016, 332);
+            this.btnAddTeam.Name = "btnAddTeam";
+            this.btnAddTeam.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTeam.TabIndex = 2;
+            this.btnAddTeam.Text = "Dodaj tim";
+            this.btnAddTeam.UseVisualStyleBackColor = true;
+            // 
+            // Teams
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAddTeam);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvTeams);
+            this.Name = "Teams";
+            this.Size = new System.Drawing.Size(1123, 476);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTeams)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTeams;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnAddTeam;
+    }
+}
