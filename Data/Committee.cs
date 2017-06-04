2a1481f4a3534a77b9ab35a435e41c6a78a77d91
Saša Hrgović <shrgovic@outlook.com>
2017-06-04 17:52:51 +0200

    Add constructor to Committee class

--- Data/Committee.cs
+++ Data/Committee.cs
@@ -11,5 +11,12 @@ namespace Data
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<User> Users { get; set; }
+        public ICollection<Team> Teams { get; set; }
+
+        public Committee()
+        {
+            Users = new HashSet<User>();
+            Teams = new HashSet<Team>();
+        }
     }
 }
