69d8a7c953e71cbf2ac42398868351c519df7930
kspanice <kspanice@foi.hr>
2017-06-12 17:53:33 +0200

    Add Meeting class

---
+++ Data/Meeting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+        [Required, StringLength(255)]
+        public string Subject { get; set; }
+        [Required, StringLength(1000)]
+        public string Description { get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+        public int MeetingTypeId { get; set; }
+        public virtual MeetingType MeetingType { get; set; }
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+        public virtual ICollection<MeetingUser> MeetingsUsers { get; set; }
+
+        public Meeting()
+        {
+            MeetingsUsers = new HashSet<MeetingUser>();
+        }
+
+
+
+    }
+}
---
+++ Data/MeetingUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class MeetingUser
+    {
+        [Key, Column(Order = 1)]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+        [Key, Column(Order = 2)]
+        public int MeetingId { get; set; }
+        public virtual Meeting Meeting { get; set; }
+    }
+}
