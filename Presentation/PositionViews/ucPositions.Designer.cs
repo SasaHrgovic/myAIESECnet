1b7cf8810171e8d3d71e049e52e7868c7d64e614
kspanice <kspanice@foi.hr>
2017-06-12 19:00:17 +0200

    Add ucPositions user control

---
+++ Presentation/PositionViews/ucPositions.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Presentation.PositionViews
+{
+    partial class ucPositions
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnUpdatePosition = new System.Windows.Forms.Button();
+            this.btnAddPosition = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.dgvPositions = new System.Windows.Forms.DataGridView();
+            this.positionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.teamIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.teamDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.roleIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.roleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usersTeamsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPositions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdatePosition
+            // 
+            this.btnUpdatePosition.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdatePosition.Location = new System.Drawing.Point(498, 306);
+            this.btnUpdatePosition.Name = "btnUpdatePosition";
+            this.btnUpdatePosition.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdatePosition.TabIndex = 7;
+            this.btnUpdatePosition.Text = "Ažuriraj";
+            this.btnUpdatePosition.UseVisualStyleBackColor = true;
+            // 
+            // btnAddPosition
+            // 
+            this.btnAddPosition.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddPosition.Location = new System.Drawing.Point(589, 306);
+            this.btnAddPosition.Name = "btnAddPosition";
+            this.btnAddPosition.Size = new System.Drawing.Size(75, 23);
+            this.btnAddPosition.TabIndex = 6;
+            this.btnAddPosition.Text = "Dodaj tim";
+            this.btnAddPosition.UseVisualStyleBackColor = true;
+            this.btnAddPosition.Click += new System.EventHandler(this.btnAddPosition_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTitle.Location = new System.Drawing.Point(16, 37);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(257, 24);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Popis timova lokalnog odbora";
+            // 
+            // dgvPositions
+            // 
+            this.dgvPositions.AllowUserToAddRows = false;
+            this.dgvPositions.AllowUserToDeleteRows = false;
+            this.dgvPositions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvPositions.AutoGenerateColumns = false;
+            this.dgvPositions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPositions.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dgvPositions.BackgroundColor = System.Drawing.Color.White;
+            this.dgvPositions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPositions.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.teamIdDataGridViewTextBoxColumn,
+            this.teamDataGridViewTextBoxColumn,
+            this.roleIdDataGridViewTextBoxColumn,
+            this.roleDataGridViewTextBoxColumn,
+            this.usersTeamsDataGridViewTextBoxColumn});
+            this.dgvPositions.DataSource = this.positionBindingSource;
+            this.dgvPositions.Location = new System.Drawing.Point(20, 73);
+            this.dgvPositions.Margin = new System.Windows.Forms.Padding(20);
+            this.dgvPositions.MultiSelect = false;
+            this.dgvPositions.Name = "dgvPositions";
+            this.dgvPositions.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvPositions.Size = new System.Drawing.Size(661, 224);
+            this.dgvPositions.TabIndex = 4;
+            // 
+            // positionBindingSource
+            // 
+            this.positionBindingSource.DataSource = typeof(Data.Position);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            // 
+            // teamIdDataGridViewTextBoxColumn
+            // 
+            this.teamIdDataGridViewTextBoxColumn.DataPropertyName = "TeamId";
+            this.teamIdDataGridViewTextBoxColumn.HeaderText = "TeamId";
+            this.teamIdDataGridViewTextBoxColumn.Name = "teamIdDataGridViewTextBoxColumn";
+            // 
+            // teamDataGridViewTextBoxColumn
+            // 
+            this.teamDataGridViewTextBoxColumn.DataPropertyName = "Team";
+            this.teamDataGridViewTextBoxColumn.HeaderText = "Team";
+            this.teamDataGridViewTextBoxColumn.Name = "teamDataGridViewTextBoxColumn";
+            this.teamDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // roleIdDataGridViewTextBoxColumn
+            // 
+            this.roleIdDataGridViewTextBoxColumn.DataPropertyName = "RoleId";
+            this.roleIdDataGridViewTextBoxColumn.HeaderText = "RoleId";
+            this.roleIdDataGridViewTextBoxColumn.Name = "roleIdDataGridViewTextBoxColumn";
+            // 
+            // roleDataGridViewTextBoxColumn
+            // 
+            this.roleDataGridViewTextBoxColumn.DataPropertyName = "Role";
+            this.roleDataGridViewTextBoxColumn.HeaderText = "Role";
+            this.roleDataGridViewTextBoxColumn.Name = "roleDataGridViewTextBoxColumn";
+            this.roleDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // usersTeamsDataGridViewTextBoxColumn
+            // 
+            this.usersTeamsDataGridViewTextBoxColumn.DataPropertyName = "UsersTeams";
+            this.usersTeamsDataGridViewTextBoxColumn.HeaderText = "UsersTeams";
+            this.usersTeamsDataGridViewTextBoxColumn.Name = "usersTeamsDataGridViewTextBoxColumn";
+            this.usersTeamsDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // ucProjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnUpdatePosition);
+            this.Controls.Add(this.btnAddPosition);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvPositions);
+            this.Name = "ucProjects";
+            this.Size = new System.Drawing.Size(701, 343);
+            this.Load += new System.EventHandler(this.ucPositions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPositions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdatePosition;
+        private System.Windows.Forms.Button btnAddPosition;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView dgvPositions;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn teamIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn teamDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn roleIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn roleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usersTeamsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource positionBindingSource;
+    }
+}
---
+++ Presentation/PositionViews/ucPositions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation.PositionViews
+{
+    public partial class ucPositions : UserControl
+    {
+        private static ucPositions _instance;
+        public static ucPositions Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new ucPositions();
+                return _instance;
+            }
+        }
+
+        public ucPositions()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPosition_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ucPositions_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
