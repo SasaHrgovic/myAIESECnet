1ac87043b6a3c9e603d4822f2e899522b9c9113c
kspanice <kspanice@foi.hr>
2017-06-12 17:53:00 +0200

    Add ConferenceUser class

---
+++ Data/ConferenceUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ConferenceUser
+    {
+        [Key, Column(Order = 1)]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+        [Key, Column(Order = 2)]
+        public int ConferenceId { get; set; }
+        public virtual Conference Conference { get; set; }
+    }
+}
