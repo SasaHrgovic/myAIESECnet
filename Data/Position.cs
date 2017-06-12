6f6610861dbf1d094d2ab1145f2008840b6fefd0
kspanice <kspanice@foi.hr>
2017-06-12 17:51:36 +0200

    Add Position class

---
+++ Data/Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(1000)]
+        public string Description { get; set; }
+        public int TeamId { get; set; }
+        public virtual Team Team { get; set; }
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public virtual ICollection<UserTeam> UsersTeams { get; set; }
+        
+        public Position()
+        {
+            UsersTeams = new HashSet<UserTeam>();
+        }        
+    }
+}
