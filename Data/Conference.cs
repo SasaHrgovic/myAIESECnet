69a60be301572f32e2950e1622f57ffdd95d8fe7
kspanice <kspanice@foi.hr>
2017-06-12 17:52:42 +0200

    Add Conference class

---
+++ Data/Conference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Conference
+    {
+        public int Id { get; set; }
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [Required, StringLength(255)]
+        public string Location { get; set; }
+        [Required, StringLength(1000)]
+        public string Description { get; set; }
+        [Required]
+        public DateTime Start { get; set; }
+        [Required]
+        public DateTime End { get; set; }
+        [Required]
+        public bool Mandatory { get; set; }
+        public virtual ICollection<ConferenceUser> ConferencesUsers { get; set; }
+
+        public Conference()
+        {
+            ConferencesUsers = new HashSet<ConferenceUser>();
+        }
+    }
+}
