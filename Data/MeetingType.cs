992fe54140f95f9eb6f30a754736efc33d331f91
kspanice <kspanice@foi.hr>
2017-06-12 17:53:51 +0200

    Add MeetingType class

---
+++ Data/MeetingType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data
+{
+    public class MeetingType
+    {
+        public int Id { get; set; }
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        public bool Mandatory { get; set; }
+        public virtual ICollection<Meeting> Meetings { get; set; }
+    }
+}
