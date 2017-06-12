23a01d8ecd3b368862ab7d87de8fc14df63a9fc3
kspanice <kspanice@foi.hr>
2017-06-12 17:51:56 +0200

    Add UserTeam class

---
+++ Data/UserTeam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UserTeam
+    {
+        [Key, Column(Order=1)]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+        [Key, Column(Order=2)]
+        public int TeamId { get; set; }
+        public virtual Team Team { get; set; }
+        [Index(IsUnique=true)]
+        public int? PositionId { get; set; } 
+        public virtual Position Position { get; set; }
+    }
+}
