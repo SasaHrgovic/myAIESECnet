81edcdd85fde800caef35d9d8b4ca0f03297ac4e
katraguz <katarinar11@gmail.com>
2017-06-14 19:15:22 +0200

    Add frmRegistration

---
+++ Presentation/frmRegistration.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Presentation
+{
+    partial class frmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.cbxDay = new System.Windows.Forms.ComboBox();
+            this.cbxMonth = new System.Windows.Forms.ComboBox();
+            this.cbxYear = new System.Windows.Forms.ComboBox();
+            this.cbxCommittee = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnRegistriraj = new System.Windows.Forms.Button();
+            this.lblPrijava = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(105, 53);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(215, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(105, 91);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(215, 20);
+            this.txtSurname.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(105, 127);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(215, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(105, 165);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(215, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(105, 204);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(215, 20);
+            this.txtPhoneNumber.TabIndex = 4;
+            // 
+            // cbxDay
+            // 
+            this.cbxDay.FormattingEnabled = true;
+            this.cbxDay.Location = new System.Drawing.Point(185, 250);
+            this.cbxDay.Name = "cbxDay";
+            this.cbxDay.Size = new System.Drawing.Size(54, 21);
+            this.cbxDay.TabIndex = 5;
+            this.cbxDay.Text = "Dan";
+            // 
+            // cbxMonth
+            // 
+            this.cbxMonth.FormattingEnabled = true;
+            this.cbxMonth.Location = new System.Drawing.Point(105, 250);
+            this.cbxMonth.Name = "cbxMonth";
+            this.cbxMonth.Size = new System.Drawing.Size(62, 21);
+            this.cbxMonth.TabIndex = 6;
+            this.cbxMonth.Text = "Mjesec";
+            this.cbxMonth.SelectedIndexChanged += new System.EventHandler(this.cbxMonth_SelectedIndexChanged);
+            // 
+            // cbxYear
+            // 
+            this.cbxYear.FormattingEnabled = true;
+            this.cbxYear.Location = new System.Drawing.Point(259, 250);
+            this.cbxYear.Name = "cbxYear";
+            this.cbxYear.Size = new System.Drawing.Size(61, 21);
+            this.cbxYear.TabIndex = 7;
+            this.cbxYear.Text = "Godina";
+            // 
+            // cbxCommittee
+            // 
+            this.cbxCommittee.FormattingEnabled = true;
+            this.cbxCommittee.Location = new System.Drawing.Point(105, 290);
+            this.cbxCommittee.Name = "cbxCommittee";
+            this.cbxCommittee.Size = new System.Drawing.Size(121, 21);
+            this.cbxCommittee.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "E-mail:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Lozinka:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Broj mobitela:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Datum rođenja:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 293);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Lokalni odbor:";
+            // 
+            // btnRegistriraj
+            // 
+            this.btnRegistriraj.Location = new System.Drawing.Point(259, 326);
+            this.btnRegistriraj.Name = "btnRegistriraj";
+            this.btnRegistriraj.Size = new System.Drawing.Size(88, 23);
+            this.btnRegistriraj.TabIndex = 16;
+            this.btnRegistriraj.Text = "Registriraj se";
+            this.btnRegistriraj.UseVisualStyleBackColor = true;
+            this.btnRegistriraj.Click += new System.EventHandler(this.btnRegistriraj_Click);
+            // 
+            // lblPrijava
+            // 
+            this.lblPrijava.AutoSize = true;
+            this.lblPrijava.Location = new System.Drawing.Point(12, 331);
+            this.lblPrijava.Name = "lblPrijava";
+            this.lblPrijava.Size = new System.Drawing.Size(86, 13);
+            this.lblPrijava.TabIndex = 17;
+            this.lblPrijava.TabStop = true;
+            this.lblPrijava.Text = "Već imaš račun?";
+            this.lblPrijava.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblPrijava_LinkClicked);
+            // 
+            // frmRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 361);
+            this.Controls.Add(this.lblPrijava);
+            this.Controls.Add(this.btnRegistriraj);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxCommittee);
+            this.Controls.Add(this.cbxYear);
+            this.Controls.Add(this.cbxMonth);
+            this.Controls.Add(this.cbxDay);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmRegistration";
+            this.Text = "frmRegistration";
+            this.Load += new System.EventHandler(this.frmRegistration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.ComboBox cbxDay;
+        private System.Windows.Forms.ComboBox cbxMonth;
+        private System.Windows.Forms.ComboBox cbxYear;
+        private System.Windows.Forms.ComboBox cbxCommittee;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnRegistriraj;
+        private System.Windows.Forms.LinkLabel lblPrijava;
+    }
+}
---
+++ Presentation/frmRegistration.cs
@@ -0,0 +1,98 @@
+﻿using Data;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation
+{
+    public partial class frmRegistration : Form
+    {
+
+        List<int[]> listArray = null;
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void lblPrijava_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRegistriraj_Click(object sender, EventArgs e)
+        {
+            User u = new User();
+            u.Name = txtName.Text;
+            u.Surname = txtSurname.Text;
+            u.Email = txtEmail.Text;
+            u.Password = txtPassword.Text;
+            u.PhoneNumber = txtPhoneNumber.Text;
+            u.BirthDate = DateTime.Parse(cbxYear.Text + "/" + cbxMonth.Text + "/" + cbxDay.Text);
+            u.CommitteeId = int.Parse(cbxCommittee.SelectedValue.ToString());
+            LoginLogic l = new LoginLogic();
+            
+            if (l.Register(u))
+            {
+                MessageBox.Show("Registracija je uspješna!", "Registracija", MessageBoxButtons.OK);
+                this.Close();
+            }
+            
+        }
+
+        private void frmRegistration_Load(object sender, EventArgs e)
+        {
+            LoginLogic l = new LoginLogic();
+            listArray = l.GetDate();
+            cbxMonth.DataSource = listArray[0];
+            cbxYear.DataSource = listArray[4];
+            cbxCommittee.DataSource = l.GetCommittees();
+            cbxCommittee.ValueMember = "Id";
+            cbxCommittee.DisplayMember = "Name";
+            
+        }
+
+        private void cbxMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int mjesec = cbxMonth.SelectedIndex+1;
+            switch (mjesec)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    {
+                        cbxDay.DataSource = null;
+                        cbxDay.DataSource = listArray[3];
+                        break;
+                    }
+                case 2:
+                    cbxDay.DataSource = null;
+                    cbxDay.DataSource = listArray[1];
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    {
+                        cbxDay.DataSource = null;
+                        cbxDay.DataSource = listArray[2];
+                        break;
+                    }
+            }
+
+
+
+        }
+    }
+}
