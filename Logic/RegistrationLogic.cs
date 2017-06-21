bac32a16093b3fd571448c0dede128135ad53d8d
katraguz <katarinar11@gmail.com>
2017-06-21 18:51:31 +0200

    Add class RegistrationLogic

---
+++ Logic/RegistrationLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Logic
+{
+    class RegistrationLogic
+    {
+        public bool Register(User u)
+        {
+            using (MyAiesecNetDbContext db = new MyAiesecNetDbContext())
+            {
+                Role r = db.Roles.Find(1);
+                u.UsersRoles.Add(new UserRole() { Role = r, User = u });
+                db.Users.Add(u);
+                db.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
