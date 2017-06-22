7e0da4d37057fc6216fa6c1770a0d13e5efd8ce7
Saša Hrgović <shrgovic@outlook.com>
2017-06-22 15:52:18 +0200

    Update add and remove methods for team members
    You can now drag & drop committee members to add or remove them from the team

--- Logic/TeamLogic.cs
+++ Logic/TeamLogic.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Data;
 using System.ComponentModel;
+using System.Data.Entity;
 
 namespace Logic
 {
     public class TeamLogic
     {
+        private List<User> _currentMembers = null;
+        private List<User> _usersToAdd = new List<User>();
+        private List<User> _usersToDelete = new List<User>();
         public List<Team> Get()
         {
             using(MyAiesecNetDbContext db = new MyAiesecNetDbContext())
@@ -31,20 +35,34 @@ namespace Logic
             }
         }
 
-        private Team AddTeamMembers(Team newTeam, List<User> teamMembers)
+        private Team DeleteTeamMembers(Team team)
         {
-            foreach (User teamMember in teamMembers)
+            foreach (User teamMember in _usersToDelete)
             {
-                newTeam.UsersTeams.Add(new UserTeam() { TeamId = newTeam.Id, UserId = teamMember.Id });
+                UserTeam ut = team.UsersTeams.Where(y => y.UserId == teamMember.Id).Single();
+                team.UsersTeams.Remove(ut);
             }
-            return newTeam;
+            return team;
+        }
+
+        private Team AddTeamMembers(Team team)
+        {
+            foreach (User teamMember in _usersToAdd)
+            {
+                team.UsersTeams.Add(new UserTeam() { TeamId = team.Id, UserId = teamMember.Id });
+            }
+            return team;
         }
 
         public void Add(Team newTeam, List<User> teamMembers)
         {
             using (MyAiesecNetDbContext db = new MyAiesecNetDbContext())
             {
-                if (teamMembers != null) newTeam = AddTeamMembers(newTeam, teamMembers);
+                if (teamMembers != null)
+                {
+                    _usersToAdd = teamMembers;
+                    newTeam = AddTeamMembers(newTeam);
+                }
                 newTeam.CommitteeId = Session.committeeId;
                 db.Teams.Add(newTeam);
                 db.SaveChanges();
@@ -53,15 +71,27 @@ namespace Logic
 
         public void Update(Team teamToUpdate, Team newTeam, List<User> teamMembers)
         {
+            _currentMembers = GetTeamMembers(teamToUpdate);
             using (MyAiesecNetDbContext db = new MyAiesecNetDbContext())
             {
-                if (teamMembers != null) teamToUpdate = AddTeamMembers(newTeam, teamMembers);
                 db.Teams.Attach(teamToUpdate);
                 teamToUpdate.Name = newTeam.Name;
                 teamToUpdate.Description = newTeam.Description;
                 teamToUpdate.Start = newTeam.Start;
                 teamToUpdate.End = newTeam.End;
                 teamToUpdate.Type = newTeam.Type;
+
+                if (teamMembers != null)
+                {
+                    _usersToDelete = _currentMembers.Except(teamMembers, new UserListEqualityComparer()).ToList();
+                    if (_usersToDelete != null)
+                        teamToUpdate = DeleteTeamMembers(teamToUpdate);
+
+                    _usersToAdd = teamMembers.Except(_currentMembers, new UserListEqualityComparer()).ToList();
+                    if (_usersToAdd != null)
+                        teamToUpdate = AddTeamMembers(teamToUpdate);
+                }
+
                 db.SaveChanges();
             }
         }
