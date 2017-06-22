c1faa57212a2a8b8469b4b750b5ece200979ffb7
Saša Hrgović <shrgovic@outlook.com>
2017-06-22 14:27:19 +0200

    Add UserListEqualityComparer

---
+++ Logic/UserListEqualityComparer.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class UserListEqualityComparer : IEqualityComparer<User>
+    {
+        public bool Equals(User x, User y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+            if (x == null || y == null)
+                return false;
+            return x.Id.Equals(y.Id);
+        }
+
+        public int GetHashCode(User obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
