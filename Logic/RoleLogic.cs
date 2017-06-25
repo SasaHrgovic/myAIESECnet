33315a870aa3dd6f91d8b5c2f68562d625c8f99d
kspanice <kspanice@foi.hr>
2017-06-25 21:04:33 +0200

    Add Role logic

---
+++ Logic/RoleLogic.cs
@@ -0,0 +1,21 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class RoleLogic
+    {
+        public static List<Role> Get()
+        {
+            using (MyAiesecNetDbContext db = new MyAiesecNetDbContext())
+            {
+                List<Role> roles = db.Roles.ToList();
+                return roles;
+            }
+        }
+    }
+}
