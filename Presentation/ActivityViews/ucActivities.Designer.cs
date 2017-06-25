029840ce48f0f372a84d636d8ac9db121c56b551
Saša Hrgović <shrgovic@outlook.com>
2017-06-26 00:54:56 +0200

    Add activities user control

---
+++ Presentation/ActivityViews/ucActivities.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Presentation.ActivityViews
+{
+    partial class ucActivities
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnUpdateActivity = new System.Windows.Forms.Button();
+            this.btnAddActivity = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.dgvActivities = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvActivities)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdateActivity
+            // 
+            this.btnUpdateActivity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdateActivity.Location = new System.Drawing.Point(870, 261);
+            this.btnUpdateActivity.Name = "btnUpdateActivity";
+            this.btnUpdateActivity.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateActivity.TabIndex = 9;
+            this.btnUpdateActivity.Text = "Ažuriraj";
+            this.btnUpdateActivity.UseVisualStyleBackColor = true;
+            // 
+            // btnAddActivity
+            // 
+            this.btnAddActivity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddActivity.Location = new System.Drawing.Point(951, 261);
+            this.btnAddActivity.Name = "btnAddActivity";
+            this.btnAddActivity.Size = new System.Drawing.Size(75, 23);
+            this.btnAddActivity.TabIndex = 8;
+            this.btnAddActivity.Text = "Dodaj";
+            this.btnAddActivity.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 16);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(88, 24);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Aktivnosti";
+            // 
+            // dgvActivities
+            // 
+            this.dgvActivities.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvActivities.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvActivities.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dgvActivities.BackgroundColor = System.Drawing.Color.White;
+            this.dgvActivities.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvActivities.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvActivities.Location = new System.Drawing.Point(16, 51);
+            this.dgvActivities.MultiSelect = false;
+            this.dgvActivities.Name = "dgvActivities";
+            this.dgvActivities.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvActivities.Size = new System.Drawing.Size(1010, 200);
+            this.dgvActivities.TabIndex = 6;
+            // 
+            // ucActivities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnUpdateActivity);
+            this.Controls.Add(this.btnAddActivity);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvActivities);
+            this.Name = "ucActivities";
+            this.Size = new System.Drawing.Size(1060, 468);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvActivities)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdateActivity;
+        private System.Windows.Forms.Button btnAddActivity;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView dgvActivities;
+    }
+}
